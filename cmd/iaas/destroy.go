52c3c35958779a02632ac7a785c86596d7c6203c
AFz <0xafz@proton.me>
2025-04-09 17:35:29 +0330

    feat: concurrent resource destruction

--- cmd/iaas/destroy.go
+++ cmd/iaas/destroy.go
@@ -2,6 +2,7 @@ package iaas
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/0xAFz/ku/internal/api"
 	"github.com/0xAFz/ku/internal/state"
@@ -18,13 +19,21 @@ var destroyCmd = &cobra.Command{
 			return
 		}
 
+		var wg sync.WaitGroup
+
 		for _, v := range current {
-			if err := provider.DeleteInstance(map[string]string{"name": v.Name}); err != nil {
-				fmt.Printf("kubar_compute_instance.%s: %v\n", v.Name, err)
-				return
-			}
-			fmt.Printf("kubar_compute_instance.%s: Destruction complete\n", v.Name)
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				if err := provider.DeleteInstance(map[string]string{"name": v.Name}); err != nil {
+					fmt.Printf("kubar_compute_instance.%s: %v\n", v.Name, err)
+					return
+				}
+				fmt.Printf("kubar_compute_instance.%s: Destruction complete\n", v.Name)
+			}()
 		}
+
+		wg.Wait()
 
 		if err := state.WriteCurrentState([]api.KubarInstance{}); err != nil {
 			fmt.Println("update current state:", err)
